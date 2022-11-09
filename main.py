f8888a93c9e3135d5267f23df0841af045c823ce
Benjamin Langer <ideale.kritzeln_0u@icloud.com>
2022-11-09 10:49:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
 
     for component in cal.walk():
         if component.name == 'VEVENT':
-            if component.get('summary') == 'Requirements Engineering und Usability SD Vorlesung':
-                cal.subcomponents.remove(component)
+            #if component.get('summary') == 'Requirements Engineering und Usability SD Vorlesung':
+            #    cal.subcomponents.remove(component)
             if component.get('summary') == 'Requirements Engineering und Usability SD Übung':
                 cal.subcomponents.remove(component)
             if component.get('summary') == 'Programmiertechnik III SD Vorlesung':
@@ -29,24 +29,24 @@ if __name__ == '__main__':
         if component.name == 'VEVENT':
             if component.get('summary') == 'Multimediatechnologie SD Vorlesung':
                 cal.subcomponents.remove(component)
+            if component.get('summary') == 'Multimediatechnologie SD Übung':
+                cal.subcomponents.remove(component)
             if component.get('summary') == 'Mathematik I SD Vorlesung':
-                cal.subcomponents.remove(component)
-            if component.get('summary') == 'Programmiertechnik I SD Vorlesung':
-                cal.subcomponents.remove(component)
-            if component.get('summary') == 'Tutorium Mathematik I SD Tutorium':
                 cal.subcomponents.remove(component)
             if component.get('summary') == 'Mathematik I SD Übung':
                 cal.subcomponents.remove(component)
-            if component.get('summary') == 'Multimediatechnologie SD Übung':
+            if component.get('summary') == 'Tutorium Mathematik I SD Tutorium':
+                cal.subcomponents.remove(component)
+            if component.get('summary') == 'Programmiertechnik I SD Vorlesung':
                 cal.subcomponents.remove(component)
             if component.get('summary') == 'Programmiertechnik I SD Übung':
                 cal.subcomponents.remove(component)
-            if component.get('summary') == 'Grundlagen der IT Hardware SD Übung':
+            if component.get('summary') == 'Tutorium Programmiertechnik I + II SD Tutorium':
                 cal.subcomponents.remove(component)
             if component.get('summary') == 'Grundlagen der IT Hardware SD Vorlesung':
                 cal.subcomponents.remove(component)
-            if component.get('summary') == 'Tutorium Programmiertechnik I + II SD Tutorium':
-                cal.subcomponents.remove(component)
+            if component.get('summary') == 'Grundlagen der IT Hardware SD Übung':
+                cal.subcomponents.remove(component)    
 
     f = open('out/SD_2022_1.ics', 'wb')
     f.write(cal.to_ical())
